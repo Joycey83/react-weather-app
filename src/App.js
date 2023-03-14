1993ef37049be2d3d35d82ca3314752460e19c56
Joycey <Jcheung8385@gmail.com>
2023-03-14 11:08:54 +0000

    I added bootstrap to my react weather project

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <h2>Are you working??</h2>
-      <footer>
-        This project was coded by Joyce Cheung and is {""}
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Open sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Joyce Cheung and is {""}
+          <a
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
