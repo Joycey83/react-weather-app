9f4fab09daaef257bf900634cbe5ebe3d1e95283
Joycey <Jcheung8385@gmail.com>
2023-03-21 14:21:16 +0000

    Added an extra day to make it a six day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -23,7 +23,7 @@ function WeatherForecast(props) {
       <div className="WeatherForecast">
         <div className="row">
           {forecast.map(function (dailyforecast, index) {
-            if (index < 5) {
+            if (index < 6) {
               return (
                 <div className="col" key={index}>
                   <WeatherForecastDay data={dailyforecast} />
